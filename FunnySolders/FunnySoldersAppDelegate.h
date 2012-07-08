d1d982de4096c3b2b0a50d29df219a0cf32ed29d
student <student@students-iMac-16.local>
2012-07-08 00:50:06 -0700

    Initial Commit

---
+++ FunnySolders/FunnySoldersAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FunnySoldersAppDelegate.h
+//  FunnySolders
+//
+//  Created by student on 7/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FunnySoldersAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FunnySolders/FunnySoldersViewController.h
@@ -0,0 +1,13 @@
+//
+//  FunnySoldersViewController.h
+//  FunnySolders
+//
+//  Created by student on 7/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FunnySoldersViewController : UIViewController
+
+@end
